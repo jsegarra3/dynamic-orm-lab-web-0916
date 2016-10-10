d9e5b5e0f7719360e3bdcb8fbfce7c12909a2eb2
James Segarra III <JamesSegarra@gmail.com>
2016-10-10 17:23:29 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,12 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  attr_accessor :name, :grade, :id
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @grade = attributes[:grade]
+    @id = attributes[:id]
+  end
+
 end
